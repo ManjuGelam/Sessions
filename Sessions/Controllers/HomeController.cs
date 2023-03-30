79ffe128ebe74ccb6ed50cf845eaa6b826d22f95
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-30 18:08:03 +0530

    Add project files.

---
+++ Sessions/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sessions.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public RedirectToRouteResult Index1(int? id,string name, int? mobilenumber)
+        {
+            Session["id"] = id;
+            Session["name"] = name;
+            Session["mobilenumber"] = mobilenumber;
+            return RedirectToAction("index2");
+        }
+        public ViewResult index2()
+        {
+            return View();
+        }
+    }
+}
